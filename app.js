b0710e70112fe4723cd06c87c3cebabde94452d2
Abdelrhman23k <71826691+Abdelrhman23k@users.noreply.github.com>
2025-09-30 15:58:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -301,7 +301,7 @@ class EgyptExplorer {
       'type': 'raster',
       'source': 'night-lights-source',
       'paint': {
-        'raster-opacity': 0.9
+        'raster-opacity': 0.5
       }
     },
     firstSymbolId // Place it below labels to keep them readable
